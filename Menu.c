901b86b875b57581f9ae15ac96ed2ae81ed05eb4
Joel Ayala <140276353+JoelAyala01@users.noreply.github.com>
2025-03-29 23:57:44 -0500

    Se sube la funcion Menu de tipo void

---
+++ Menu.c
@@ -0,0 +1,12 @@
+
+void Menu()
+{
+    printf("\n\n************AYALA BANK************\n\n");
+    printf("1. Generar Ticket\n");
+    printf("2. Atender Cliente\n");
+    printf("3. Consultar cantidad de clientes cajeros\n");
+    printf("4. Listar Clientes En Cola\n");
+    printf("5. Salir\n");
+    printf("Seleccionar Opcion: ");
+    return;
+}
